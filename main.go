5f457851df2ba76351c56f49e2e7aa27bacc17b3
tattsun <t.t.mc1192.sf@gmail.com>
2018-10-27 20:49:07 +0900

    impl

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+var (
+	count uint64
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" && r.Method == http.MethodGet {
+		atomic.AddUint64(&count, 1)
+		cnt := atomic.LoadUint64(&count)
+		fmt.Fprintf(w, "%d", cnt)
+	}
+}
+
 func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
